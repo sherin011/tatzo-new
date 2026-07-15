58d9db61fdfec0aa381fd8877e057be22b93f996
sherin011 <akasherin008@gmail.com>
2026-07-15 15:18:26 +0530

    fix: utilize official build properties abstraction parameters wrapper inside local plugin setup

--- withModularHeaders.js
+++ withModularHeaders.js
@@ -1,38 +1,20 @@
-const { withDangerousMod } = require('@expo/config-plugins');
-const fs = require('fs');
-const path = require('path');
+const { withBuildProperties } = require('expo-build-properties');
 
-function withModularHeaders(config) {
-  return withDangerousMod(config, [
-    'ios',
-    async (config) => {
-      const podfilePath = path.join(config.modRequest.platformProjectRoot, 'Podfile');
-      let podfileContent = fs.readFileSync(podfilePath, 'utf8');
-
-      const patchCode = [
-        '    # Force bypass non-modular header compiler errors for firebase modules',
-        '    installer.pods_project.targets.each do |target|',
-        "      if target.name.start_with?('RNFBApp') || target.name.start_with?('react-native-firebase')",
-        '        target.build_configurations.each do |config|',
-        "          config.build_settings['CLANG_ALLOW_NON_MODULAR_INCLUDES_IN_FRAMEWORK_MODULES'] = 'YES'",
-        '        end',
-        '      end',
-        '    end'
-      ].join('\n');
-
-      if (/post_install\s+do\s+\|[a-zA-Z_]+\|/.test(podfileContent)) {
-        podfileContent = podfileContent.replace(
-          /(post_install\s+do\s+\|([a-zA-Z_]+)\|)/,
-          '$1\n    installer = $2\n' + patchCode
-        );
-      } else {
-        podfileContent += '\npost_install do |installer|\n' + patchCode + '\nend\n';
-      }
-
-      fs.writeFileSync(podfilePath, podfileContent);
-      return config;
-    },
-  ]);
-}
-
-module.exports = withModularHeaders;
+module.exports = function withModularHeaders(config) {
+  // Use Expo's official build properties plugin mapping wrapper under the hood
+  return withBuildProperties(config, {
+    ios: {
+      useFrameworks: 'static',
+      extraPods: [
+        {
+          name: 'FirebaseCore',
+          modular_headers: true
+        },
+        {
+          name: 'FirebaseCoreInternal',
+          modular_headers: true
+        }
+      ]
+    }
+  });
+};
